e6ec6e83f08b6f585e2a1e6ab1b8a10a593d20c3
SANTIAGO LOPEZ <santiagolopezslz60@gmail.com>
2023-10-02 17:29:43 -0300

    ..

--- frmProveedores.cs
+++ frmProveedores.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using System.IO;
+using System.Data;
 
 namespace pryLopezIEvaluativa
 {
